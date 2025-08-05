0b1ec408ab58bc4216b3d7ed590c424e01f178ce
KINCHERUSKA <kryovv718@gmail.com>
2025-08-06 01:26:58 +0300

    added a base logic

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,18 +2,30 @@
 defineProps({
   title: String,
   imageUrl: String,
+  isFavorite: Boolean,
+  isAdded: Boolean,
+  onClickAdd: Function,
+  onClickFavorite: Function,
 })
 </script>
 
 <template>
   <div
-    class="cursor-pointer py-8 md:py-12 hover:-translate-y-2 transition relative font-raleway text-lg md:text-2xl font-normal flex flex-col items-center w-full max-w-[400px] gap-[10px] hover:shadow-2xl hover:z-10"
+    class="cursor-pointer py-8 md:py-12 hover:-translate-y-2 transition relative font-raleway text-lg md:text-2xl font-normal flex flex-col items-center w-full gap-[10px] hover:shadow-2xl hover:z-10"
   >
-    <img src="/like-2.svg" alt="" class="z-20 absolute top-8 left-8 cursor-pointer transition" />
+    <img
+      @click="onClickFavorite"
+      :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+      alt=""
+      class="z-20 absolute top-8 left-8 cursor-pointer transition"
+    />
     <img :src="imageUrl" class="" alt="Sneakers" />
-    <span class="text-wrap text-center w-[80%]">{{ title }}</span>
-    <div class="border border-black px-3 md:px-4 lg:px-6 py-1 md:py-2 whitespace-nowrap w-fit">
-      <span>в корзину</span>
+    <span class="text-wrap text-center w-full px-4 break-words">{{ title }}</span>
+    <div
+      @click="onClickAdd"
+      class="border border-black px-3 md:px-4 lg:px-6 py-1 md:py-2 whitespace-nowrap w-fit"
+    >
+      <span>{{ isAdded ? 'добавлено' : 'в корзину' }}</span>
     </div>
   </div>
 </template>
--- src/components/CardL.vue
+++ src/components/CardL.vue
@@ -1,13 +1,33 @@
 <script setup>
 import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('Добавлено!')
+}
+
+const onClickFavorite = () => {
+  alert('Сохранено!')
+}
 </script>
 
 <template>
-  <div class="grid grid-cols-3 gap-8">
-    <Card title="Casual Sneakers" image-url="/sneakers/sneakers-1.jpg" />
-    <Card />
-    <Card />
-    <Card />
+  <div
+    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 md:gap-12 lg:gap-20"
+  >
+    <Card
+      :is-favorite="true"
+      :is-added="true"
+      title="Casual Sneakers"
+      image-url="/sneakers/sneakers-1.jpg"
+      :onClickAdd="onClickAdd"
+      :onClickFavorite="onClickFavorite"
+    />
+    <Card
+      :is-favorite="false"
+      :is-added="false"
+      title="Casual Sneakers"
+      image-url="/sneakers/sneakers-2.jpg"
+    />
     <Card />
     <Card />
   </div>
